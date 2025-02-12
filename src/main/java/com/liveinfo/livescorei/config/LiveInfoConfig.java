fea1477adbc2490faa5cb4b501ee317040f468d6
Jeevan Patel <jeevan.v.patel@gmail.com>
2025-02-12 15:49:21 +0530

    25

--- src/main/java/com/liveinfo/livescorei/config/LiveInfoConfig.java
+++ src/main/java/com/liveinfo/livescorei/config/LiveInfoConfig.java
@@ -71,7 +71,7 @@ public class LiveInfoConfig {
 
     	return List.of(
     			new MatchBoard.Builder().withId("bf12ec10-a6e7-43b1-8cf6-6e2fcb4321f9")//"38c3deb9-4562-45b5-9eef-94b06f8badde")
-    			.withT1("England [ENG]").withT2("India [IND]").withDateTimeGMT("02/12/2025 07:30:00").withTStamp(LiveInfoConstant.CURR_TS).build()
+    			.withT1("England [ENG]").withT2("India [IND]").withDateTimeGMT("02/12/2025 08:00:00").withTStamp(LiveInfoConstant.CURR_TS).build()
     			);
     }
     
